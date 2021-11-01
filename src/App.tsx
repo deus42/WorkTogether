c0d058c8e0730391d10aaf393260de4a03a69594
Alexey Gapchenko <deusson@gmail.com>
2021-11-01 10:53:34 +0200

    Upgraded packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import "regenerator-runtime/runtime";
-import React from "react";
-import { login, logout } from "./utils";
-import "./global.css";
+import 'regenerator-runtime/runtime'
+import React from 'react'
+import { login, logout } from './utils'
+import './global.css'
 
-import getConfig from "./config";
-const { networkId } = getConfig(process.env.NODE_ENV || "development");
+import getConfig from './config'
+const { networkId } = getConfig(process.env.NODE_ENV || 'development')
 
 export default function App() {
   // use React Hooks to store greeting in component state
-  const [greeting, set_greeting] = React.useState();
+  const [greeting, set_greeting] = React.useState()
 
   // when the user has not yet interacted with the form, disable the button
-  const [buttonDisabled, setButtonDisabled] = React.useState(true);
+  const [buttonDisabled, setButtonDisabled] = React.useState(true)
 
   // after submitting the form, we want to show Notification
-  const [showNotification, setShowNotification] = React.useState(false);
+  const [showNotification, setShowNotification] = React.useState(false)
 
   // The useEffect hook can be used to fire side-effects during render
   // Learn more: https://reactjs.org/docs/hooks-intro.html
@@ -26,8 +26,8 @@ export default function App() {
         window.contract
           .get_greeting({ account_id: window.accountId })
           .then((greetingFromContract) => {
-            set_greeting(greetingFromContract);
-          });
+            set_greeting(greetingFromContract)
+          })
       }
     },
 
@@ -35,7 +35,7 @@ export default function App() {
     // Use an empty array to specify "only run on first render"
     // This works because signing into NEAR Wallet reloads the page
     []
-  );
+  )
 
   // if not signed in, return early with sign-in prompt
   if (!window.walletConnection.isSignedIn()) {
@@ -53,17 +53,17 @@ export default function App() {
           to other currencies – they're just for testing!
         </p>
         <p>Go ahead and click the button below to try it out:</p>
-        <p style={{ textAlign: "center", marginTop: "2.5em" }}>
+        <p style={{ textAlign: 'center', marginTop: '2.5em' }}>
           <button onClick={login}>Sign in</button>
         </p>
       </main>
-    );
+    )
   }
 
   return (
     // use React Fragment, <>, to avoid wrapping elements in unnecessary divs
     <>
-      <button className="link" style={{ float: "right" }} onClick={logout}>
+      <button className="link" style={{ float: 'right' }} onClick={logout}>
         Sign out
       </button>
       <main>
@@ -71,73 +71,73 @@ export default function App() {
           <label
             htmlFor="greeting"
             style={{
-              color: "var(--secondary)",
-              borderBottom: "2px solid var(--secondary)",
+              color: 'var(--secondary)',
+              borderBottom: '2px solid var(--secondary)',
             }}
           >
             {greeting}
           </label>
           {
-            " " /* React trims whitespace around tags; insert literal space character when needed */
+            ' ' /* React trims whitespace around tags; insert literal space character when needed */
           }
           {window.accountId}!
         </h1>
         <form
           onSubmit={async (event) => {
-            event.preventDefault();
+            event.preventDefault()
 
             // get elements from the form using their id attribute
-            const { fieldset, greeting } = event.target.elements;
+            const { fieldset, greeting } = event.target.elements
 
             // hold onto new user-entered value from React's SynthenticEvent for use after `await` call
-            const newGreeting = greeting.value;
+            const newGreeting = greeting.value
 
             // disable the form while the value gets updated on-chain
-            fieldset.disabled = true;
+            fieldset.disabled = true
 
             try {
               // make an update call to the smart contract
               await window.contract.set_greeting({
                 // pass the value that the user entered in the greeting field
                 message: newGreeting,
-              });
+              })
             } catch (e) {
               alert(
-                "Something went wrong! " +
-                  "Maybe you need to sign out and back in? " +
-                  "Check your browser console for more info."
-              );
-              throw e;
+                'Something went wrong! ' +
+                  'Maybe you need to sign out and back in? ' +
+                  'Check your browser console for more info.'
+              )
+              throw e
             } finally {
               // re-enable the form, whether the call succeeded or failed
-              fieldset.disabled = false;
+              fieldset.disabled = false
             }
 
             // update local `greeting` variable to match persisted value
-            set_greeting(newGreeting);
+            set_greeting(newGreeting)
 
             // show Notification
-            setShowNotification(true);
+            setShowNotification(true)
 
             // remove Notification again after css animation completes
             // this allows it to be shown again next time the form is submitted
             setTimeout(() => {
-              setShowNotification(false);
-            }, 11000);
+              setShowNotification(false)
+            }, 11000)
           }}
         >
           <fieldset id="fieldset">
             <label
               htmlFor="greeting"
               style={{
-                display: "block",
-                color: "var(--gray)",
-                marginBottom: "0.5em",
+                display: 'block',
+                color: 'var(--gray)',
+                marginBottom: '0.5em',
               }}
             >
               Change greeting
             </label>
-            <div style={{ display: "flex" }}>
+            <div style={{ display: 'flex' }}>
               <input
                 autoComplete="off"
                 defaultValue={greeting}
@@ -147,7 +147,7 @@ export default function App() {
               />
               <button
                 disabled={buttonDisabled}
-                style={{ borderRadius: "0 5px 5px 0" }}
+                style={{ borderRadius: '0 5px 5px 0' }}
               >
                 Save
               </button>
@@ -161,12 +161,12 @@ export default function App() {
         <ol>
           <li>
             Look in <code>src/App.tsx</code> and <code>src/utils.js</code> –
-            you'll see <code>get_greeting</code> and <code>set_greeting</code>{" "}
+            you'll see <code>get_greeting</code> and <code>set_greeting</code>{' '}
             being called on <code>contract</code>. What's this?
           </li>
           <li>
-            Ultimately, this <code>contract</code> code is defined in{" "}
-            <code>assembly/main.ts</code> – this is the source code for your{" "}
+            Ultimately, this <code>contract</code> code is defined in{' '}
+            <code>assembly/main.ts</code> – this is the source code for your{' '}
             <a
               target="_blank"
               rel="noreferrer"
@@ -177,20 +177,20 @@ export default function App() {
             .
           </li>
           <li>
-            When you run <code>yarn dev</code>, the code in{" "}
+            When you run <code>yarn dev</code>, the code in{' '}
             <code>assembly/main.ts</code> gets deployed to the NEAR testnet. You
             can see how this happens by looking in <code>package.json</code> at
-            the <code>scripts</code> section to find the <code>dev</code>{" "}
+            the <code>scripts</code> section to find the <code>dev</code>{' '}
             command.
           </li>
         </ol>
         <hr />
         <p>
-          To keep learning, check out{" "}
+          To keep learning, check out{' '}
           <a target="_blank" rel="noreferrer" href="https://docs.near.org">
             the NEAR docs
-          </a>{" "}
-          or look through some{" "}
+          </a>{' '}
+          or look through some{' '}
           <a target="_blank" rel="noreferrer" href="https://examples.near.org">
             example apps
           </a>
@@ -199,12 +199,12 @@ export default function App() {
       </main>
       {showNotification && <Notification />}
     </>
-  );
+  )
 }
 
 // this component gets rendered by App after the form is submitted
 function Notification() {
-  const urlPrefix = `https://explorer.${networkId}.near.org/accounts`;
+  const urlPrefix = `https://explorer.${networkId}.near.org/accounts`
   return (
     <aside>
       <a
@@ -215,9 +215,9 @@ function Notification() {
         {window.accountId}
       </a>
       {
-        " " /* React trims whitespace around tags; insert literal space character when needed */
+        ' ' /* React trims whitespace around tags; insert literal space character when needed */
       }
-      called method: 'set_greeting' in contract:{" "}
+      called method: 'set_greeting' in contract:{' '}
       <a
         target="_blank"
         rel="noreferrer"
@@ -230,5 +230,5 @@ function Notification() {
         <div>Just now</div>
       </footer>
     </aside>
-  );
+  )
 }
