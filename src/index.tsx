880d0915020e337d775c51b067a7f2f78cb175d8
Alexey Gapchenko <deusson@gmail.com>
2021-11-01 10:19:50 +0200

    Moving to typescript

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { initContract } from "./utils";
+
+//@ts-ignore
+window.nearInitPromise = initContract()
+  .then(() => {
+    ReactDOM.render(<App />, document.querySelector("#root"));
+  })
+  .catch(console.error);
